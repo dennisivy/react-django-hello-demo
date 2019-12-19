8bdf4e4a3952cfef7f3962492b1c5653baced00f
divanov11 <33843378+divanov11@users.noreply.github.com>
2019-12-18 17:53:09 -0800

    Add files via upload

---
+++ react_django/react-app/build/precache-manifest.4a62b965672bbd46c091f82b704f19d9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d74a9fb4b9c18628bbe9eacfcc66c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e175b31f769abb44b56",
+    "url": "/static/css/main.920e1f78.chunk.css"
+  },
+  {
+    "revision": "66067e2e05849021a3d1",
+    "url": "/static/js/2.28888999.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.28888999.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e175b31f769abb44b56",
+    "url": "/static/js/main.70c8eeab.chunk.js"
+  },
+  {
+    "revision": "fbaf048f868100b869bd",
+    "url": "/static/js/runtime-main.8f658356.js"
+  },
+  {
+    "revision": "6e7bb28791921c949c57e1bd2e244ff9",
+    "url": "/static/media/files.6e7bb287.PNG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d544ffe6bf3d6c758b104d72968f5ed1",
+    "url": "/static/media/node_modules.d544ffe6.PNG"
+  },
+  {
+    "revision": "975cdc14fbafb3aecde694c7ffd6718d",
+    "url": "/static/media/staticfiles.975cdc14.PNG"
+  },
+  {
+    "revision": "0d544eb6875337969631204ebc4a7ff6",
+    "url": "/static/media/templates.0d544eb6.PNG"
+  },
+  {
+    "revision": "1c2ac42311709d0b5e396c7960a4dbc8",
+    "url": "/static/media/urls.1c2ac423.PNG"
+  }
+]);
